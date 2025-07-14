3f84c65738a5d0d6cc6d5de2c7df0cfdf6fdb240
Mohamed <mohamedabdo150250@gmail.com>
2025-07-14 22:54:54 +0300

    اول مشورع

---
+++ main.js
@@ -0,0 +1,91 @@
+// Initialize AOS (Animate On Scroll)
+AOS.init({
+  duration: 1000,
+  offset: 100,
+  once: false,
+});
+
+// Header scroll effect
+const header = document.querySelector("header");
+const links = document.querySelectorAll("nav ul li a");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+    links.forEach((link) => {
+      link.classList.add("scrolled");
+    });
+  } else {
+    header.classList.remove("scrolled");
+    links.forEach((link) => {
+      link.classList.remove("scrolled");
+    });
+  }
+});
+
+const darkToggle = document.querySelector(".dark-mode");
+const icon = darkToggle.querySelector("i");
+const home = document.querySelector(".hero");
+// Load saved mode on page load
+const savedMode = localStorage.getItem("color");
+if (savedMode === "dark") {
+  header.classList.add("dark");
+  home.classList.add("dark");
+  icon.classList.remove("fa-moon");
+  icon.classList.add("fa-sun");
+  icon.style.color = "#cea637ff";
+}
+
+// Toggle dark mode
+darkToggle.addEventListener("click", () => {
+  if (icon.classList.contains("fa-moon")) {
+header.classList.add("dark");
+    home.classList.add("dark");
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+    icon.style.color = "#cea637ff";
+    localStorage.setItem("color", "dark");
+  } else {
+    header.classList.remove("dark");
+    home.classList.remove("dark");
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+    icon.style.color = "#1a1a1a";
+    localStorage.setItem("color", "light");
+  }
+});
+// toggle menu
+const menuBtn = document.querySelector(".menu-btn i");
+const menu = document.querySelector(".menu-btn");
+menu.addEventListener("click", () => {
+  if (menuBtn.classList.contains("fa-bars")) {
+    menuBtn.classList.remove("fa-bars");
+    menuBtn.classList.add("fa-x");
+  } else {
+    menuBtn.classList.remove("fa-x");
+    menuBtn.classList.add("fa-bars");
+  }
+});
+
+//import Emailjs
+import emailjs from 'https://cdn.jsdelivr.net/npm/@emailjs/browser@4.1.0/+esm';
+
+emailjs.init("IxTvCs3lBPrkfs0c2"); // ← public key بتاعك
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const result = await emailjs.sendForm(
+      "service_c2it3u6",       // ← Service ID
+      "template_c8mlyv5",      // ← Template ID
+      form
+    );
+    alert("✅ تم إرسال الرسالة بنجاح");
+    form.reset();
+  } catch (error) {
+    console.error("❌ فشل الإرسال:", error);
+    alert("❌ فشل إرسال الرسالة. راجع الكونسول.");
+  }
+});
+
